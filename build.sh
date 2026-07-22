36186d1b7edafc35257b5439abf3ad337113e8ee
Amit Dutta <mail@amit.is-a.dev>
2026-07-22 12:48:22 +0530

    Update SVG icons and navigation links in build.sh

--- build.sh
+++ build.sh
@@ -130,8 +130,8 @@ echo "$REPOS_JSON" | jq -c '.' | while read -r repo_info; do
     [ -z "$REPO_SIZE" ] && REPO_SIZE="unknown"
     ACTIVITY_SVG=$(generate_stats_svg "$REPOS_DIR/$REPO.git")
 
-    BACK_ARROW_SVG='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>'
-    NAV_LINKS="[<a href=\"files.html\" class=\"nav-arrow\">${BACK_ARROW_SVG} Root</a>] | Activity"
+    BACK_ARROW_SVG='<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-left-icon lucide-move-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>'
+    NAV_LINKS="<a href=\"files.html\" class=\"nav-arrow\">${BACK_ARROW_SVG}</a>"
 
     {
         echo "<!DOCTYPE html><html><head><meta charset=\"utf-8\">"
@@ -139,6 +139,7 @@ echo "$REPOS_JSON" | jq -c '.' | while read -r repo_info; do
         echo "<h1>$REPO</h1><p class=\"desc\">$DESC</p>"
         echo "<p>$NAV_LINKS</p><hr>"
         echo "<div id=\"activity-meta\">"
+		echo "<h2>Activity record of $USERNAME/$REPO</h2>"
         echo "<p>GitHub: <a href=\"https://github.com/$USERNAME/$REPO\" target=\"_blank\">github.com/$USERNAME/$REPO</a></p>"
         echo "<p>Clone: <code>git clone https://github.com/$USERNAME/$REPO.git</code></p>"
         echo "<p>Last commit: <a href=\"https://github.com/$USERNAME/$REPO/commit/$LAST_COMMIT_HASH\" target=\"_blank\">$LAST_COMMIT_SHORT</a> ($LAST_COMMIT_TIME)</p>"
